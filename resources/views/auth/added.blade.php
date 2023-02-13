756213061448444a85c2eb352de993c40a5ab412
yuyaK <babyshamblespc0@gmail.com>
2023-02-13 20:40:20 +0900

    start

---
+++ resources/views/auth/added.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.logout')
+
+@section('content')
+
+<div id="clear">
+<p>〇〇さん、</p>
+<p>ようこそ！DAWNSNSへ！</p>
+<p>ユーザー登録が完了しました。</p>
+<p>さっそく、ログインをしてみましょう。</p>
+
+<p class="btn"><a href="/login">ログイン画面へ</a></p>
+</div>
+
+@endsection
