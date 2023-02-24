ef90e4b9de716f1d1043a8abf6f7e378b8ee6b47
yuyaK <babyshamblespc0@gmail.com>
2023-02-25 02:25:44 +0900

    -

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,17 @@
 {!! Form::open() !!}
 
 <p>DAWNSNSへようこそ</p>
-
-{{ Form::label('e-mail') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
-{{ Form::label('password') }}
-{{ Form::password('password',['class' => 'input']) }}
-
-{{ Form::submit('ログイン') }}
+<div class='form-text'>
+  <ul>
+    <li>{{ Form::label('Mail Adress') }}
+      {{ Form::text('mail',null,['class' => 'input']) }}
+    </li>
+    <li>{{ Form::label('Password') }}
+      {{ Form::password('password',['class' => 'input']) }}
+    </li>
+  </ul>
+</div>
+{{ Form::submit('LOGIN') }}
 
 <p><a href="/register">新規ユーザーの方はこちら</a></p>
 
